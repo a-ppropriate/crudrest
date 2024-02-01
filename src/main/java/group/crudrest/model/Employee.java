ad634c850b64585e2a4943f72ae226298af0e3db
a-ppropriate <>
2024-02-01 18:08:56 +0700

    Remove task listing from the employee endpoint

--- src/main/java/group/crudrest/model/Employee.java
+++ src/main/java/group/crudrest/model/Employee.java
@@ -2,6 +2,8 @@ package group.crudrest.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -45,6 +47,7 @@ public class Employee {
 	}
 
 	@OneToMany(mappedBy = "employee")
+	@JsonIgnore
 	private List<Task> tasks;
 
 	public Long getId() {
