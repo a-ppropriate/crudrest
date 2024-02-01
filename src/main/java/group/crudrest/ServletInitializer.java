480270db915742146b0883e6ed8ad42b97490e77
a-ppropriate <>
2024-02-01 14:32:06 +0700

    Initial commit

---
+++ src/main/java/group/crudrest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package group.crudrest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CrudrestApplication.class);
+	}
+
+}
---
+++ src/test/java/group/crudrest/CrudrestApplicationTests.java
@@ -0,0 +1,13 @@
+package group.crudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
